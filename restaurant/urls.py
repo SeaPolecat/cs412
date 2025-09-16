dfd5f1c27abb6f99f1c4a88832c258a07c905a12
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-09-16 11:19:36 -0400

    added new files for restaurant app

---
+++ restaurant/urls.py
@@ -0,0 +1,16 @@
+# File: restaurant/urls.py
+# Author: Yi Ji (Wayne) Wang (waynew@bu.edu), 9/12/2025
+# Description: Contains urls for the quotes web app that redirect to
+# functions within views.py.
+
+from django.urls import path
+from django.conf import settings
+from . import views
+
+# list containing different url endings that redirect to functions within views.py
+urlpatterns = [
+    path(r'', views.quote, name="main"),
+    path(r'quote', views.quote, name="quote"),
+    path(r'show_all', views.show_all, name="show_all"),
+    path(r'about', views.about, name="about"),
+]
---
+++ restaurant/views.py
@@ -0,0 +1,9 @@
+# File: restaurant/views.py
+# Author: Yi Ji (Wayne) Wang (waynew@bu.edu), 9/12/2025
+# Description: Contains urls for the quotes web app that redirect to
+# functions within views.py.
+
+from django.shortcuts import render
+
+# Create your views here.
+
