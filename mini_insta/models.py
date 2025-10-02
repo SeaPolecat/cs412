5c5605082d80e24c1081d7aaeab4979557a86640
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-10-02 10:52:55 -0400

    added accessor methods and changed timestamp fields to have autonow be true

--- mini_insta/models.py
+++ mini_insta/models.py
@@ -19,6 +19,14 @@ class Profile(models.Model):
 
         return f'{self.username} ({self.display_name})'
     
+    def get_all_posts(self):
+        """Return a QuerySet of all Posts on this Profile."""
+
+        # posts are ordered by newest first
+        posts = Post.objects.filter(profile=self).order_by('-timestamp')
+
+        return posts
+    
 
 class Post(models.Model):
     """Encapsulate the data of a post on a Mini Insta profile."""
@@ -26,13 +34,19 @@ class Post(models.Model):
     # the foreign key to indicate the relationship to the Profile of the creator of this post
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     caption = models.TextField(blank=True) # the optional text associated with this post
-    timestamp = models.DateTimeField(blank=True) # the time at which this post was created/saved
+    timestamp = models.DateTimeField(auto_now=True) # the time at which this post was created/saved
 
     def __str__(self):
         """Return a string representation of this Post model instance."""
 
-        return f'{self.profile}: {self.caption}'
+        return f'{self.profile} | {self.caption}'
     
+    def get_all_photos(self):
+        """Return a QuerySet of all Photos on this Post."""
+    
+        photos = Photo.objects.filter(post=self).order_by('-timestamp')
+
+        return photos
 
 class Photo(models.Model):
     """Encapsulate the data of an image on a Mini Insta post."""
@@ -40,9 +54,9 @@ class Photo(models.Model):
     # the foreign key to indicate the relationship to the Post to which this Photo is associated
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     image_url = models.URLField(blank=True) # a valid URL to an image stored on the public world-wide web
-    timestamp = models.DateTimeField(blank=True) # the time at which this Photo was created/saved
+    timestamp = models.DateTimeField(auto_now=True) # the time at which this Photo was created/saved
 
     def __str__(self):
         """Return a string representation of this Photo model instance."""
 
-        return f'{self.post}: {self.image_url}'
+        return f'{self.post.profile} | {self.post.caption} | Photo {self.pk}'
