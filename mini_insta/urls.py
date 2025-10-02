5d3643476e46f3d4c41d8c485938b32952bffeac
Yi Ji (Wayne) Wang <wangwayne400@gmail.com>
2025-10-02 11:53:35 -0400

    added PostDetailView and its url

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -4,10 +4,11 @@
 # classes within views.py.
 
 from django.urls import path
-from .views import ProfileListView, ProfileDetailView
+from .views import *
 
 # list containing different url endings that redirect to classes within views.py
 urlpatterns = [
     path('', ProfileListView.as_view(), name="show_all_profiles"),
     path('profile/<int:pk>', ProfileDetailView.as_view(), name="show_profile"),
+    path('post/<int:pk>', PostDetailView.as_view(), name="show_post"),
 ]
